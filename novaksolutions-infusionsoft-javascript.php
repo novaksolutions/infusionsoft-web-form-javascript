32f51cf5cd55fad17cedff1916ff38d8dae36ac7
Jacob Allred <jacob.allred@corbanworks.com>
2014-02-05 11:33:17 -0500

    Initial commit on GitHub

---
+++ novaksolutions-infusionsoft-javascript.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+Plugin Name: Infusionsoft® Web Form JavaScript
+Plugin URI: http://novaksolutions.com/wordpress-plugins/infusionsoft-webform-javascript/
+Description: Easily insert Infusionsoft® web forms into your posts or pages.
+Author: Novak Solutions
+Version: 1.1.0
+Author URI: http://novaksolutions.com/
+*/
+
+function novaksolutions_wf_shortcode($attributes) {
+    $return = '';
+
+    if (!empty($attributes['css'])) {
+        $return .= "<div class=\"javascript-container\" style=\"{$attributes['css']}\"><script type=\"text/javascript\" src=\"{$attributes['src']}\"></script></div>";
+
+    } else {
+        $return .= "<script type=\"text/javascript\" src=\"{$attributes['src']}\"></script>";
+    }
+
+    return $return;
+}
+add_shortcode(get_option('novaksolutions_wf_setting_shortcode', 'javascript'), 'novaksolutions_wf_shortcode');
+
+
+function novaksolutions_wf_replace_js($data, $postarr = null) {
+    // Define string parts that we are looking to replace
+    $search = array(
+        '&lt;script type=\"text/javascript\" src=\"',
+        '\"&gt;&lt;/script&gt;',
+    );
+    
+    // Define replacement string parts
+    $replace = array(
+        '[' . get_option('novaksolutions_wf_setting_shortcode', 'javascript') . ' src="',
+        '"/]',
+    );
+    
+    // Perform search/replace
+    $data['post_content'] = str_replace($search, $replace, $data['post_content']);
+    
+    return $data;
+}
+if (get_option('novaksolutions_wf_setting_autoreplace', 1)) {
+    add_filter('wp_insert_post_data', 'novaksolutions_wf_replace_js');
+}
+
+
+function novaksolutions_wf_plugin_action_links( $links, $file ) {
+    if ( $file == plugin_basename( dirname(__FILE__).'/novaksolutions-infusionsoft-javascript.php' ) ) {
+        $links[] = '<a href="' . admin_url( 'admin.php?page=novaksolutions-wf-settings' ) . '">'.__( 'Settings' ).'</a>';
+    }
+
+    return $links;
+}
+add_filter('plugin_action_links', 'novaksolutions_wf_plugin_action_links', 10, 2);
+
+function novaksolutions_wf_admin_menu() {
+    $page_title = 'Infusionsoft® Web Form JavaScript Settings';
+    $menu_title = 'Web Form JavaScript';
+    $capability = 'manage_options';
+    $menu_slug = 'novaksolutions-wf-settings';
+    $function = 'novaksolutions_wf_settings';
+    add_options_page($page_title, $menu_title, $capability, $menu_slug, $function);
+}
+add_action('admin_menu', 'novaksolutions_wf_admin_menu');
+
+function novaksolutions_wf_settings() {
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permissions to access this page.');
+    }
+    
+    echo '<h2>Infusionsoft® Web Form JavaScript Settings</h2>';
+    
+    echo '<form method="POST" action="options.php">';
+    settings_fields('novaksolutions-wf-settings');   //pass slug name of page
+    do_settings_sections('novaksolutions-wf-settings');    //pass slug name of page
+    submit_button();
+    echo '</form>';
+
+    echo '<h3>Like this plugin?</h3>';
+    echo '<p>Visit <a href="http://novaksolutions.com/?utm_source=wordpress&utm_medium=link&utm_campaign=wf">Novak Solutions</a> to find dozens of free tips, tricks, and tools to help you get the most out of Infusionsoft®.</p>';
+}
+
+
+function novaksolutions_wf_settings_api_init() {
+    // Add the section to reading settings so we can add our
+    // fields to it
+    add_settings_section('novaksolutions_wf_setting_section',
+        null,
+        null,
+        'novaksolutions-wf-settings');
+    
+    // Add the field with the names and function to use for our new
+    // settings, put it in our new section
+    add_settings_field('novaksolutions_wf_setting_shortcode',
+        'Shortcode',
+        'novaksolutions_wf_setting_callback_function_shortcode',
+        'novaksolutions-wf-settings',
+        'novaksolutions_wf_setting_section');
+
+    add_settings_field('novaksolutions_wf_setting_autoreplace',
+        'Auto Replace',
+        'novaksolutions_wf_setting_callback_function_autoreplace',
+        'novaksolutions-wf-settings',
+        'novaksolutions_wf_setting_section');
+    
+    // Register our setting so that $_POST handling is done for us and
+    // our callback function just has to echo the <input>
+    register_setting('novaksolutions-wf-settings','novaksolutions_wf_setting_shortcode', 'novaksolutions_wf_setting_shortcode_sanitize');
+    register_setting('novaksolutions-wf-settings','novaksolutions_wf_setting_autoreplace');
+}
+
+add_action('admin_init', 'novaksolutions_wf_settings_api_init');
+
+function novaksolutions_wf_setting_shortcode_sanitize($string) {
+    // Shortcode should, ideally, consist ONLY of lowercase letters.
+    $string = preg_replace( '/[^a-z]/i', "", sanitize_title($string, 'javascript'));
+    if(empty($string)) {
+        $string = 'javascript';
+    }
+
+    return $string;
+}
+
+function novaksolutions_wf_setting_callback_function_shortcode() {
+    echo '<input name="novaksolutions_wf_setting_shortcode" type="text" value=" ' . get_option('novaksolutions_wf_setting_shortcode', 'javascript') . '" class="code" /> <p class="description">You can change the default shortcode, <strong>javascript</strong>, to something else if needed. This doesn\'t update any posts or pages that are currently using the original shortcode.</p>';
+}
+
+function novaksolutions_wf_setting_callback_function_autoreplace() {
+    echo '<input name="novaksolutions_wf_setting_autoreplace" type="checkbox" value="1" class="code" ' . checked( 1, get_option('novaksolutions_wf_setting_autoreplace', 1), false ) . ' /> Automatically convert JavaScript to use the shortcode';
+}
+
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Infusionsoft Web Form JavaScript ===
+Contributors: zulugrid, joeynovak
+Tags: infusionsoft, webform, web forms, webforms, javascript
+Author URI: http://novaksolutions.com/
+Plugin URI: http://novaksolutions.com/wordpress-plugins/infusionsoft-webform-javascript/
+Requires at least: 2.7
+Tested up to: 3.8
+Stable tag: 1.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily add Infusionsoft web forms to your posts and pages. Automatically converts JavaScript to WordPress-friendly shortcodes.
+
+== Description ==
+
+We make it easy to add Infusionsoft web forms to your blog. You simply copy and paste the web form JavaScript into your post or page, and this plugin will automatically convert it to a WordPress shortcode that won't be mangled by WordPress.
+
+By default, the plugin lets you use the shortcode *javascript*. Unlike other plugins, we let you change this shortcode to anything you like, such as *webform* or *infusionsoft*.
+
+You can even add custom CSS to your web form. Simply add a CSS attribute to the shortcode, for example:
+
+`[javascript css="float: left;" src="https://example.infusionsoft.com/app/form/iframe/5d07ccaa3e9ab94dea1f6982da9fb266"/]`
+
+**Want to do even more with Infusionsoft?** Novak Solutions has expert developers and ready-to-go tools to make your life easier. [Visit Novak Solutions now!](http://novaksolutions.com/)
+
+**Check us out on Facebook!** Our latest Infusionsoft tools and plugin updates are always posted on Facebook. Check us out at http://www.facebook.com/novaksolutions/
+
+== Frequently Asked Questions ==
+
+= How do I change the shortcode? =
+
+Go to the plugin's settings page to change the shortcode to anything you'd like. Please be aware that changing the shortcode won't update your existing posts, so it is best to pick a shortcode before using the plugin and to stick to it.
+
+= How do I add custom CSS to my web form? =
+
+Add a CSS attribute to your shortcode, for example:
+
+`[javascript css="float: left;" src="https://example.infusionsoft.com/app/form/iframe/5d07ccaa3e9ab94dea1f6982da9fb266"/]`
+
+= How do I keep JavaScript from automatically being converted to shortcodes? =
+
+You can easily disable this feature on the plugin's settings page.
+
+== Changelog ==
+
+= 1.1.0 =
+* First release on WordPress.org
